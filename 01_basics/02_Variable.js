00b94d0355068a78f8b444779ad6c23bbe6cfab6
Aman Raj <84539100+aman26195@users.noreply.github.com>
2024-10-01 13:55:25 +0000

    Datatype

---
+++ 01_basics/02_Variable.js
@@ -0,0 +1,17 @@
+let name="aman"
+let age=24
+let isLogged=true
+let state=null;
+// number =>2 to power 53
+// bigint
+// string=>""
+// boolean=>true ya false
+// null standlone value ha 
+// undefined =>jab you have not defined a variable
+// symbol=>unique
+
+
+
+// object
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
