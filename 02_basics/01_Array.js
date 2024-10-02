29eedba0a950366882238455e120adffa64c523c
Aman Raj <84539100+aman26195@users.noreply.github.com>
2024-10-02 16:35:09 +0000

    array

---
+++ 02_basics/01_Array.js
@@ -0,0 +1,21 @@
+//array
+const myArr=[0,3,2,6,8,4,2]
+console.log(myArr[0]);
+//array method
+myArr.push(6)
+myArr.push(7)
+// console.log(myArr);
+// myArr.unshift(9)
+// myArr.shift()
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(9));
+
+// console.log(myArr);
+//slice,splish
+console.log("A",myArr);
+const myn1=myArr.slice(1,3)
+console.log(myn1);
+console.log("B",myArr);
+
+
+
