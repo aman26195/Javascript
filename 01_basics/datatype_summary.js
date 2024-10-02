d70e06a974e134f9f5323c61342002a42c1f96a7
Aman Raj <84539100+aman26195@users.noreply.github.com>
2024-10-02 08:45:57 +0000

    datatype Summary

---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,23 @@
+// primitive
+// 7 type: String ,Boolean,Number,null,undefined,symbol,BigInt
+constscore=100
+const scoreValue=100.3
+const isLoggedIn=false
+const outsideTemp=null
+let userEmail;
+// symbol
+const id=Symbol('123'
+)
+const anotherid=Symbol('123')
+console.log(id==anotherid)
+// Reference type (non primitive)-array,object,function
+
+const heros=["shaktiman","naagraj","doga"]
+let myObj={
+    name:"aman",
+    age:17,
+}
+const myFunction=function()
+{
+    console.log("hello world");
+}
