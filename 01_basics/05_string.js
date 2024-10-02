562f6930f03fa40c8416e399289721c470968d17
Aman Raj <84539100+aman26195@users.noreply.github.com>
2024-10-02 11:25:05 +0000

    String detail

---
+++ 01_basics/05_string.js
@@ -0,0 +1,21 @@
+const name="aman"
+const repoCount=50
+// console.log(name+repoCount+"Value");
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameCount=new String('aman')
+console.log(gameCount.charAt(2));
+console.log(gameCount.indexOf('n'));
+const newString=gameCount.substring(0,3);
+console.log(newString);
+const anotherString=gameCount.slice(-3,1)
+
+console.log(anotherString);
+const newStringOne="   aman  "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+const url="https://github.com/dashtop"
+url.replace(url.replace('gi','-'))
+
+
+
