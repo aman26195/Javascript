bd71e286c87d180713ac17c964d47c73d016210c
Aman Raj <84539100+aman26195@users.noreply.github.com>
2024-10-02 16:09:36 +0000

    date and time

---
+++ 01_basics/07_date.js
@@ -0,0 +1,11 @@
+//date
+let myDate=new Date()
+console.log(myDate.toString());
+console.log(myDate.toLocaleString());
+let myCraetedDate=new Date(2023,0,23)
+console.log(myCraetedDate.toDateString());
+let myTimeStamp=Date.now()
+console.log(myTimeStamp);
+let newDate=new Date()
+console.log(newDate.getDate());
+console.log(newDate.getDay());
