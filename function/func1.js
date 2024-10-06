818f54c5deccf07ec3b35eb9e04ffe92fe4b5f79
Aman Raj <84539100+aman26195@users.noreply.github.com>
2024-10-06 09:14:51 +0000

    function

---
+++ function/func1.js
@@ -0,0 +1,35 @@
+// function loginUserMessage(username="sam"){
+//     if(username===undefined){
+//         console.log("please enter a user name");
+//         return 
+
+//     }
+//     return `${username} just logged in`
+
+// }
+// console.log(loginUserMessage());
+function calculateCardPrice(val1,val2,...num1){
+    return num1
+}
+
+console.log(calculateCardPrice(200,300,500,2000));
+
+const user={
+    username:"aman",
+    price:199
+}
+function handleObject(anyobject){
+    console.log(`username is${anyobject.username} and price is ${anyobject.price}`);
+
+}
+
+// handleObject(user)
+const myNewArray=[200,300,400]
+function returnSecondValue(getArray)
+{
+    return getArray[1]
+}
+console.log(returnSecondValue(myNewArray
+    
+));
+
