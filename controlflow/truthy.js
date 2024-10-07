596105371ad875279b21e6f7a50f719f46a6313b
Aman Raj <84539100+aman26195@users.noreply.github.com>
2024-10-07 08:38:02 +0000

    control flow

---
+++ controlflow/truthy.js
@@ -0,0 +1,36 @@
+// const userEmail=[7,4,5]
+// if(userEmail){
+//     console.log("got user email");
+// }
+// else{
+//     console.log("not got user email");
+    
+// }
+// falsy value  
+// false ,0,-0,BigInt 0n " ",null undefined NaN  
+// truthy value
+// // "0",'false'," ",[],{},function(){}
+// if (userEmail.length===0)
+// {
+//     console.log("array is empty");
+    
+// }
+// const emptyObj={}
+
+// if (Object.keys(emptyObj).length===0)
+// {
+//     console.log("object is empty");
+    
+// }
+// Nullish coaleshing operator (??):null undefine
+let val1;
+// val1=5??10
+// val1=null??10
+// val1=undefined??15
+val1=null??undefined??15
+console.log(val1);
+//terniary operator
+// condition? true:false
+const iceTeaPrice=100
+iceTeaPrice>=80?console.log("less than 80"):console.log("more than 80")
+;
