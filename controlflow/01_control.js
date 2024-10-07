e834cb03b937ddb5582e6b2a4e4a16a17128e2a5
Aman Raj <84539100+aman26195@users.noreply.github.com>
2024-10-07 08:16:08 +0000

    switch and if

---
+++ controlflow/01_control.js
@@ -0,0 +1,68 @@
+// // if 
+// const isUserLoggedIn=true
+// const temperature=41
+// if(temperature===40 ){
+//     console.log("less than 50");
+// }
+// else{
+// console.log("temperature is not 50");
+// }
+//  console.log("excuted");
+ 
+// // scope
+// const score=200
+// if(score>100){
+//     const power="fly"
+
+// console.log(`User power:${power}`);
+// }
+// const balance=10000
+// // if(balance >500) console.log("test"),
+// // console.log("test2");
+// if(balance>500)
+// {
+//     console.log("less than 1000");
+    
+// }
+// else if(balance<750)
+// {
+//    console.log("less than 750");
+    
+// }
+// else{
+//     console.log("less than 1200");
+    
+// }
+// const userLoggedIn=true
+// const debitCard=true 
+// const LoggedInFromGoogle=false;
+// const LoggedInFromEmail=true;
+// if(userLoggedIn&&debitCard )
+// {
+//     console.log("allow to buy courses");
+    
+// }
+// if(LoggedInFromGoogle ||LoggedInFromEmail){
+//     console.log("user logged in");
+    
+
+// }
+const month=5
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+        case 2:
+            console.log("Jfeb"); 
+            break;
+
+            case 3:
+        console.log("march");
+        break;
+
+    default:
+        console.log("defalut case match");
+        
+        break;
+}
+
